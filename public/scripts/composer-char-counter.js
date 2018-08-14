aca988ce1caaeb887b55da833230058c9c3f5b4f
Alexander Horvath <tantousha@me.com>
2018-08-14 23:50:47 +0000

    created character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
-  var textarea = document.querySelector('textarea[name=text]');
-  textarea.addEventListener('keydown', function(event) {
-    console.log(this);
+  $("#tweetbox").on('keyup', function() {
+    var characterCount = $(this).val().length;
+    var charactersRemaining = 140 - characterCount;
+    $(this).siblings('.counter').text(charactersRemaining);
+    if (charactersRemaining < 0) {
+      $('.counter').css("color","red");
+    }
+    if (charactersRemaining >= 0) {
+      $('.counter').css("color", "black");
+    }
   })
 });
