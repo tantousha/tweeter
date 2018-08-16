7191cc294ffbc6bfee37e61de1dc6189f1da0f42
Alexander Horvath <tantousha@me.com>
2018-08-16 20:25:34 +0000

    finished sample mongodb server

--- mongo_example.js
+++ mongo_example.js
@@ -11,5 +11,24 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
 
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
+  function  getTweets(callback) {
+    db.collection("tweets").find().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, tweets);
+    });
+  }
+
+  getTweets((err, tweets) => {
+    if (err) throw err;
+
+    console.log("Logging each tweet: ");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    }
+
     db.close();
+  });
+
 });
