bbad5f45aae76f12dcdaf746ed6c7019d5b745d2
Alexander Horvath <tantousha@me.com>
2018-08-17 00:06:59 +0000

    removed hardcoded database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,22 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetsDatabase = {
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
-
 $(function() {
   var $tweets = $('#old_tweets');
 
